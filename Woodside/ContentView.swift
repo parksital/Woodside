8419ee133453a53a7b6a469cac6100dfb8f4c67f
Parv Sital <rsital@icloud.com>
2019-10-20 12:29:01 +0200

    Initial Commit

---
+++ Woodside/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Woodside
+//
+//  Created by Parvin Sital on 20/10/2019.
+//  Copyright © 2019 Parvin Sital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
