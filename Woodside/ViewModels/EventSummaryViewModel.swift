fc4090554f2f7b74220a1687b2a983f2daa3b964
Parv Sital <rsital@icloud.com>
2020-01-17 17:11:49 +0100

    can't even properly rename a struct lmao

---
+++ Woodside/ViewModels/EventSummaryViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  EventSummaryViewModel.swift
+//  Woodside
+//
+//  Created by Parvin Sital on 17/01/2020.
+//  Copyright © 2020 Parvin Sital. All rights reserved.
+//
+
+import Foundation
+
+struct EventSummaryViewModel: Identifiable {
+    let id: String
+    let name: String
+    let startDate: String
+    let venue: String
+}
+
+extension EventSummaryViewModel: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case startDate = "start_date"
+        case venue
+        
+        enum VenueKeys: CodingKey {
+            case name
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let venueContainer = try container.nestedContainer(keyedBy: CodingKeys.VenueKeys.self, forKey: .venue)
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        startDate = try container.decode(String.self, forKey: .startDate)
+        venue = try venueContainer.decode(String.self, forKey: .name)
+    }
+}
+
+//extension EventResponse {
+//    func getEvent(dateFormatting: (Date) -> String) -> Event {
+//        return Event(
+//            id: self.id,
+//            name: self.name,
+//            venueName: self.venueName,
+//            date: dateFormatting(self.date),
+//            description: self.description
+//        )
+//    }
+//}
